03c2ccbe2cf9fe55d467fe1a8f13b7d91d900edf
Ivan <ivan.karabankov@gmail.com>
2024-06-01 22:13:40 +0300

    2. Started making Main. Created first version of animation

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,16 @@ import * as functions from './modules/functions.js';
 // Checking if browsers supports .webp
 functions.isWebp();
 
+// Changing language on btn press
+const langBtn = document.querySelector('.menu__control_btn');
+const textSpan = document.querySelector('.langText');
+
+langBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  if (textSpan.innerHTML === 'RU') {
+    textSpan.innerHTML = 'EN';
+  } else {
+    textSpan.innerHTML = 'RU';
+  }
+});
